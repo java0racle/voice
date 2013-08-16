c07464769ae719f432f3133db0948b8c0383303f
java0racle <java0racle@163.com>
2013-08-16 10:56:30 +0800

    flash

--- pages/javascript/detail.js
+++ pages/javascript/detail.js
@@ -1,48 +1,53 @@
-
-
-define(['{lib}util/flash/flash.js'],
+define(['{lib}util/flash/flash.js',
+        '{lib}util/event.js'],
 function(){
   var _  = NEJ.P,
       _e = _('nej.e'),
       _v = _('nej.v'),
+      _p = _('vc.p'),
+      _ut= _('nej.ut'),
       _id = 'flash-obj',
-      _state = 0;
-  var _flashobj = document.embeds[_id]||_e._$get(_id)||document[_id]||window[_id];
-  var _onPlay = function(_event){
+      _state = 0,
+      _pro,
+      _sup;
+  _p._$$Voice = NEJ.C();
+  _pro = _p._$$Voice._$extend(_ut._$$Event);
+
+  _pro.__init = function(){
+    this.__supInit();
+    this.__flashobj = document.embeds[_id]||_e._$get(_id)||document[_id]||window[_id];
+    _v._$addEvent(_e._$get('play'),'click',this.__onPlay._$bind(this));
+    _v._$addEvent(_e._$get('stop'),'click',this.__onStop._$bind(this));
+    _v._$addEvent(_e._$get('mark'),'click',this.__onMark._$bind(this));
+    this.__doInit();
+  };
+
+  _pro.__doInit = function(){
+    this.__flashobj.setSrc('res/hmm.mp3');
+    this.__flashobj.load();
+  };
+
+  _pro.__onPlay = function(_event){
     _event = _event||window.event;
     var _target = _event.target||_event.srcElement;
-    if(!_flashobj||!_flashobj.play) return;
+    if(!this.__flashobj||!this.__flashobj.play) return;
     if(_state == 1){
-      _flashobj.pause();
+      this.__flashobj.pause();
       _state = 0;
       _target.value = '继续';
     }else{
-      _flashobj.play();
+      this.__flashobj.play();
       _state = 1;
       _target.value = '暂停';
     }
   };
 
-  var _onInit = function(){
-    if(!_flashobj||!_flashobj.setSrc) return;
-    _flashobj.setSrc('res/hmm.mp3');
-    _flashobj.load();
-    window.clearInterval(_t);
+  _pro.__onStop = function(){
+    if(!this.__flashobj||!this.__flashobj.stop) return;
+    this.__flashobj.stop();
   };
 
-  var _onStop = function(){
-    if(!_flashobj||!_flashobj.stop) return;
-    _flashobj.stop();
+  _pro.__onMark = function(){
   };
 
-  var _onMark = function(){
-  };
-
-  _v._$addEvent(_e._$get('play'),'click',_onPlay._$bind(this));
-  // _v._$addEvent(_e._$get('pause'),'click',_onPause._$bind(this));
-  _v._$addEvent(_e._$get('stop'),'click',_onStop._$bind(this));
-  _v._$addEvent(_e._$get('mark'),'click',_onMark._$bind(this));
-  var _t = window.setInterval(function(){
-    _onInit();
-  },1000);
 })
